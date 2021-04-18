60ecc3de725ccd91bce4cdd7936b9225e754c1f9
Victor Ribeiro <vvictorrib@gmail.com>
2021-04-18 18:12:50 -0300

    Header component

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,12 +1,28 @@
 import Logo from '../../assets/logo.png'
 import LogoWhite from '../../assets/logo-white.png'
 
-const Header = ({whiteVersion}) => {
+import { Link } from 'react-router-dom';
+import './styles.css';
+
+const Header = ({ whiteVersion, hideCart }) => {
+  const openDrawer = () => {
+    const event = new CustomEvent('openCart');
+    window.dispatchEvent(event);
+  }
+
   return (
     <div className="col-12">
       <header className="py-4 px-4 text-center">
-        <img src={whiteVersion ? LogoWhite : Logo } alt='logo' className='img-fluid' />
+        <Link to='/'>
+          <img src={whiteVersion ? LogoWhite : Logo} alt='logo' className='img-fluid' />
+        </Link>
       </header>
+      {!hideCart && (
+        <button onClick={() => openDrawer()} className='btn btn-secondary cart-button'>
+          <span className='mdi mdi-cart'></span>
+        2 Ítens
+        </button>
+      )}
     </div>
   );
 }
