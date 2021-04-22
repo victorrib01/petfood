0915e8bde2989d61baccfdde0a26e234b67ea71e
Victor Ribeiro <vvictorrib@gmail.com>
2021-04-22 14:20:57 -0300

    better ux with sidebar

--- src/components/sidebar/index.js
+++ src/components/sidebar/index.js
@@ -7,7 +7,7 @@ import { useHistory } from 'react-router-dom';
 
 const Sidebar = () => {
   const history = useHistory();
-  const {cart} = useSelector(state => state.shop)
+  const { cart } = useSelector(state => state.shop)
   const total = cart.reduce((total, product) => {
     return total + product.price;
   }, 0);
@@ -28,14 +28,17 @@ const Sidebar = () => {
       <div className="container-fluid h-100 pt-4 sidebar">
         <h5>Minha Sacola ({cart.length})</h5>
         <div className='row products'>
-          {cart.map((p) => <Product product={p}/>)}
+          {cart.map((p) => <Product product={p} />)}
         </div>
         <div className="row footer align-items-end">
           <div className="col-12 d-flex justify-content-between align-items-center">
             <b className="d-inline-block">Total</b>
             <h3 className="d-inline-block">R$ {total.toFixed(2)}</h3>
           </div>
-          <button onClick={()=> history.push('/register')} className="btn btn-block btn-lg btn-primary rounded-0 h-50 align-items-center">Finalizar Compra</button>
+          <button onClick={() => {
+            history.push('/register')
+            setOpened(false)
+          }} className="btn btn-block btn-lg btn-primary rounded-0 h-50 align-items-center">Finalizar Compra</button>
         </div>
       </div>
     </Dock>
